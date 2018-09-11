814fcdeda7ff053cb781c17edb92925f9279ecd8
Dandi T <kakatake515@gmail.com>
2018-09-11 18:40:39 +0800

    initial comit

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import WeatherReducer from './reducer_weather';
-
-const rootReducer = combineReducers({
-  weather: WeatherReducer
-  //state: (state = {}) => state
-});
-
-export default rootReducer;
